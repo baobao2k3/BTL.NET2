b1aa638248460837a10afd5a0267868ee27aed9b
TranDucVuong2003 <tranvuong310103@gmail.com>
2023-11-25 10:40:24 +0700

    create header home page

---
+++ WebMVC/WebMVC/Models/Navbar.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WebMVC.Data;
+
+namespace WebMVC.Models
+{
+	public class Navbar: ViewComponent
+	{
+		private readonly WebMVCContext _context;
+
+		public Navbar(WebMVCContext context)
+		{
+			_context = context;
+		}
+		public IViewComponentResult Invoke()
+		{
+			return View(_context.Category.ToList());
+		}
+	}
+}
